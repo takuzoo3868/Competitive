e5b647b67ac5028a7bb4867de5cd60d36ef0e19a
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-04-26 01:56:52 +0900

    :tada: Solved problem001

---
+++ problem001/solution001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+  int sum = 0;
+
+  for (int i=1; i<1000; ++i) {
+    if (!(i%3) || !(i%5)) {
+      sum += i;
+    }
+  }
+  std::cout << sum << std::endl;
+  
+  return 0;
+}
