67592daa16ae4c521f0ecabf147869a3b4fdd920
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-05-07 22:17:59 +0900

    :+1: Solved problem007

--- src/solution007.cpp
+++ src/solution007.cpp
@@ -1,4 +1,47 @@
-//
-// Created by user on 5/4/2017.
-//
+/*
+ * Problem 7: 10001st prime
+ * Find the 10,001st prime number
+ * 10,001番目の素数を出力
+ * Author: takuzoo3868
+ * Last Modified: 04/May/2017
+ */
 
+#include <iostream>
+#include <cmath>
+
+#define ORDINAL 10001
+
+using namespace std;
+
+bool IsPrime(int num) {
+    if (num < 2) return false;
+    else if (num == 2) return true;
+    else if (num % 2 == 0) return false;
+
+    double sqrtNum = sqrt(num);
+    for (int i = 3; i <= sqrtNum; i += 2) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int solution007() {
+    int integer = 0; // 整数を格納
+    int count = 0;   // 素数をカウント
+    int prime;       // 素数を格納
+
+    while (1) {
+        if (IsPrime(integer)) {
+            prime = integer;
+            count++;
+        }
+        integer++;
+        if (count == ORDINAL) break;
+    }
+    cout << prime << endl;
+
+    return 0;
+}
