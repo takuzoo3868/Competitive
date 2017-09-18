44e7fee44db5359ee46c877457ddb252a0e933dc
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-09-18 21:55:44 +0900

    :+1: Solved ABC001A
    Signed-off-by: takuzoo3868 <sata3868.k.serpentis@gmail.com>

---
+++ Atcoder/ABC/abc001/A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    int h1, h2;
+
+    cin >> h1;
+    cin >> h2;
+
+    cout << h1 - h2 << endl;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Competitive Programming
+This repo contains my solutions from various contests and online judge. Almost all of them in c++.
