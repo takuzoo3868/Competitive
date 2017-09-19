fc572ab731c7fb4c6ca2ad660818785d54e1fa71
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-09-19 21:50:12 +0900

    :+1: Solved ABC002A
    Signed-off-by: takuzoo3868 <sata3868.k.serpentis@gmail.com>

---
+++ Atcoder/ABC/abc002/A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    if (a > b) {
+        cout << a << endl;
+    } else {
+        cout << b << endl;
+    }
+
+    return 0;
+}
