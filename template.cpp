8aa19ee5376b3e649dcf214a3e71b224cd0858ce
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-09-19 22:13:27 +0900

    :recycle: Update template
    Signed-off-by: takuzoo3868 <sata3868.k.serpentis@gmail.com>

--- template.cpp
+++ template.cpp
@@ -2,9 +2,12 @@
 #include<vector>
 #include<algorithm>
 
+#define REP(i, n) for(int i=0; i<(int)(n); ++i)
+#define FOR(i, k, n) for(int i=(k);i<(int)(n);++i)
+
 using namespace std;
 
-int main(){
+int main() {
 
     return 0;
 }
